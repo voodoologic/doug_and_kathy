130cdf7393866f130d2e11fca57716055bfec5e1
voodoologic <headley.douglas@gmail.com>
2019-04-17 18:28:56 +0000

    almost working

--- start.sh
+++ start.sh
@@ -1,9 +1,15 @@
-docker kill $(docker ps -q -a)
-docker rm $(docker ps -q -a)
+docker kill $(docker ps -qa)
+docker rm $(docker ps -qa)
+# docker-compose \
+#   -p doug_and_kathy \
+#   -f ${PWD}/owncloud/docker-compose.yml \
+#   up -d \
+#   --force-recreate
 docker-compose \
   -p doug_and_kathy \
+  -f ${PWD}/owncloud/docker-compose.yml \
   -f ./docker-jitsi-meet/docker-compose.yml \
   -f ${PWD}/nginx/docker-compose.yml \
   up -d \
-  --force-recreate \
+  --force-recreate
 
